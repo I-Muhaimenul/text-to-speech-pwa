28be479217482cfadcf0bffd836570855def3930
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-09-30 00:08:20 +0600

    updated speak listen feature

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 // Init SpeechSynth API
 const synth = window.speechSynthesis;
 
+// SpeechRecognition based on Firefox || Chrome
+window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+
+// Init SpeechRecognition API
+let recognition = null;
+if ('SpeechRecognition' in window) {
+  // speech recognition API supported
+  recognition = new window.SpeechRecognition();
+
+  // setting recogntion config properties
+  recognition.interimResults = true;
+  recognition.maxAlternatives = 10;
+  recognition.continuous = true;
+}
+
 // DOM Elements
 const textForm = document.querySelector('form');
 const textInput = document.querySelector('#text-input');
@@ -11,17 +26,21 @@ const pitch = document.querySelector('#pitch');
 const pitchValue = document.querySelector('#pitch-value');
 const body = document.querySelector('body');
 
+const listenBtn = document.querySelector('#listen'); 
+
 //Browser identifier
 // Firefox 1.0+
 var isFirefox = typeof InstallTrigger !== 'undefined';
 
 // Chrome 1+
-var isChrome = !!window.chrome && !!window.chrome.webstore;
+// var isChrome = !!window.chrome && !!window.chrome.webstore;
+var isChrome = (!!window.chrome && !!window.chrome.webstore) || (/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor))
 
 // Init voices array
 let voices = [];
 
 const getVoices = () => {
+
   voices = synth.getVoices();
 
   // Loop through voices and create an option for each one
@@ -56,6 +75,9 @@ if (isChrome) {
 
 // Speak
 const speak = () => {
+  // stop listening
+  stopListen()
+
   // Check if speaking
   if (synth.speaking) {
     console.error('Already speaking...');
@@ -118,3 +140,52 @@ pitch.addEventListener('change', e => (pitchValue.textContent = pitch.value));
 
 // Voice select change
 voiceSelect.addEventListener('change', e => speak());
+
+
+// Speech recognition
+const startListen = () => {
+  if (!recognition) {
+    console.error('Recognition not found...');
+    return;
+  }
+  let finalTranscript = '';
+
+    // initiate recognition with microphone permission
+    recognition.onresult = (event) => {
+        // const speechToText = event.results[0][0].transcript;
+      let interimTranscript = '';
+      for (let i = event.resultIndex, len = event.results.length; i < len; i++) {
+        let transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) {
+          finalTranscript += transcript;
+        } else {
+          interimTranscript += transcript;
+        }
+      }
+      textInput.value = finalTranscript + interimTranscript 
+      // console.log(finalTranscript , interimTranscript )
+    }
+    recognition.start();
+}
+
+const stopListen = () => {
+  console.log('Done listening')
+  recognition.stop()
+  listenBtn.innerHTML = 'Listen It'
+    // console.log(recognition.stop())
+}
+
+// clicked on listen button
+listenBtn.addEventListener('click', e => {
+  // listenBtn.text('wee')
+  let btnText = listenBtn.innerHTML
+  if(btnText == 'Listen It') {
+    startListen();
+    listenBtn.innerHTML = 'Listening ...'
+  } else {
+    stopListen();
+    listenBtn.innerHTML = 'Listen It' 
+  }
+  // console.log(listenBtn.innerHTML)
+  // alert(23)
+});
--- js/newsApi.js
+++
@@ -1,5 +0,0 @@
-const apiKey = '54fe76a8c27843c58e8f0564176a9f8d';
-
-const topHeadlinesUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
-
-export { apiKey, topHeadlinesUrl };
