1b6ba368981942021771f8c3709ed0d8effeaec7
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2019-09-29 13:42:05 +0600

    source to root

---
+++ js/newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = '54fe76a8c27843c58e8f0564176a9f8d';
+
+const topHeadlinesUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
+
+export { apiKey, topHeadlinesUrl };
